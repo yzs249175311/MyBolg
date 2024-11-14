1f71d9bd665d916bff3b2f0759298cdae480d889
yuanzishuai <yuanzishuai@qq.com>
2024-11-14 10:54:49 +0800

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1>hello world</h1>
+      <Button
+        variant={"outline"}
+        onClick={async (e) => {
+          alert("hello world");
+        }}
+      >
+        Test Button
+      </Button>
     </div>
   );
 }
